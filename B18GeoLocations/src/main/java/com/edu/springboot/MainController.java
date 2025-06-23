4ecd57e507eb0ef36fd7ced8f4baf6ae52db8a52
yahoozhf <yahoozhf@naver.com>
2025-06-23 17:32:19 +0900

    9일차

--- B18GeoLocations/src/main/java/com/edu/springboot/MainController.java
+++ B18GeoLocations/src/main/java/com/edu/springboot/MainController.java
@@ -1,5 +1,7 @@
 package com.edu.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.edu.springboot.mybatis.ISearchRadius;
+import com.edu.springboot.mybatis.MyFacilityDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -42,5 +47,36 @@ public class MainController {
 		model.addAttribute("apikey", apikey);
 		return "03MyLocation";
 	}
+	@GetMapping("/04SearchRadius.do")
+	public String geoFunc4(Model model, HttpServletRequest req) {
+		
+		model.addAttribute("apikey" , apikey);
+		
+		int distance = (req.getParameter("distance")==null) ? 0 : Integer.parseInt(req.getParameter("distance"));
+		double latTxt = (req.getParameter("latTxt")==null) ? 0 : Double.parseDouble(req.getParameter("latTxt"));
+		double lngTxt = (req.getParameter("lngTxt")==null) ? 0 : Double.parseDouble(req.getParameter("lngTxt"));
+		
+		int numberPerPage = 200;
+		
+		int resultCount = dao.searchCount(distance, latTxt, lngTxt); 
+		
+		model.addAttribute("resultCount", " / 검색결과:" + resultCount + "건");
+		model.addAttribute("selectNum",  Math.ceil(resultCount / numberPerPage));
+		
+		int pageNum =(req.getParameter("pageNum")==null) ? 1: Integer.parseInt(req.getParameter("pageNum"));
+		int start = ((pageNum-1) * numberPerPage) + 1;
+		int end = pageNum * numberPerPage;
+		
+		System.out.println(distance + " "+ latTxt + " " + lngTxt + " " + start + " " + end);
+		
+		ArrayList<MyFacilityDTO> searchList = new ArrayList<>(); 
+		
+		if (distance != 0 && resultCount > 0) { 
+			searchList = dao.searchRadius(distance, latTxt, lngTxt, start, end);
+		}
+		
+		model.addAttribute("searchList", searchList);
+		
+		return "04SearchRadius";
+	}
 }
- 
